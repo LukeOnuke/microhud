8c3ac69d642b14b19a4f1414d6f2be832ebdc4f4
Luka Kresoja <57074484+LukeOnuke@users.noreply.github.com>
2022-07-17 03:28:15 +0200

    Update MHList.java

--- src/main/java/com/lukeonuke/minihud/gui/list/MHList.java
+++ src/main/java/com/lukeonuke/minihud/gui/list/MHList.java
@@ -2,24 +2,16 @@ package com.lukeonuke.minihud.gui.list;
 
 import com.lukeonuke.minihud.MicroHud;
 import com.lukeonuke.minihud.MicroHudColors;
-import com.lukeonuke.minihud.data.MicroHudOptions;
 import com.lukeonuke.minihud.renderer.MicroHudRenderer;
 import com.lukeonuke.minihud.renderer.module.MicroHudRendererModule;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenMouseEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.Mouse;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.Element;
-import net.minecraft.client.gui.screen.pack.PackScreen;
 import net.minecraft.client.gui.widget.AlwaysSelectedEntryListWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
-import org.jetbrains.annotations.Nullable;
-
-import java.time.Instant;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class MHList extends AlwaysSelectedEntryListWidget<MHList.Entry> {
     public MHList(MinecraftClient client, int width, int height, int top, int bottom, int itemHeight) {
@@ -59,15 +51,6 @@ public class MHList extends AlwaysSelectedEntryListWidget<MHList.Entry> {
         private final MHList widget;
         private final MinecraftClient client = MinecraftClient.getInstance();
 
-        private boolean isMouseClicked;
-        private double mX, mY;
-
-        private MHList otherList;
-        private int padding = MicroHud.PADDING;
-
-        private float tickD = 0F;
-        boolean toggleDown = true; //its bad programming, but it works, unlike THE INTENDED WAY aah
-
         public Entry(boolean showName, boolean movableControls, MicroHudRendererModule microHudRenderer, TextRenderer textRenderer, MHList widget) {
             this.showName = showName;
             this.movableControls = movableControls;
@@ -75,11 +58,11 @@ public class MHList extends AlwaysSelectedEntryListWidget<MHList.Entry> {
             this.textRenderer = textRenderer;
             this.widget = widget;
 
-            ScreenMouseEvents.afterMouseClick(client.currentScreen).register((screen, mouseX, mouseY, button) -> {
-                if (button == 0) isMouseClicked = true;
-                mX = mouseX;
-                mY = mouseY;
-            });
+//            ScreenMouseEvents.afterMouseClick(client.currentScreen).register((screen, mouseX, mouseY, button) -> {
+//                if (button == 0) isMouseClicked = true;
+//                mX = mouseX;
+//                mY = mouseY;
+//            });
         }
 
         public MicroHudRendererModule getRendererModule() {
@@ -98,57 +81,18 @@ public class MHList extends AlwaysSelectedEntryListWidget<MHList.Entry> {
 
         @Override
         public void render(MatrixStack matrices, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
-            tickD += tickDelta;
-            int u = textRenderer.getWidth("U");
-
             DrawableHelper.fill(matrices, x, y, x + entryWidth, x + entryHeight, MicroHudColors.BLUE);
-            hovered = isHovered(x, y, entryWidth, entryHeight, mouseX, mouseY);
-
-            if (movableControls) {
-                textRenderer.draw(matrices, "U", x + padding * 2 + padding, y, MicroHudColors.GREEN);
-                textRenderer.draw(matrices, "D", x + padding * 2 + u * 2 + padding * 2, y, MicroHudColors.GREEN);
-            }
+            //hovered = isHovered(x, y, entryWidth, entryHeight, mouseX, mouseY);
 
             int color = 0xFFFFFF;
             if (hovered) {
-
                 color = MicroHudColors.HOVER;
-
-                if (isMouseClicked) {
-                    int cx = (int)mouseX - widget.getRowLeft() - padding * 2;
-
-                    if (movableControls) {
-                        isMouseClicked = false;
-                        if (cx > padding && cx < padding + u) {
-                            switchLists(index, index - 1);
-                        }
-                        if (cx > (padding + u) * 2 && cx < (padding + u) * 2 + u) {
-                            //if(downCounter >= 2) {
-                            if(toggleDown){
-                                switchLists(index, index + 1);
-                                tickD = 0F;
-                            }
-                            MicroHud.LOGGER.info("{}", toggleDown);
-                            toggleDown = !toggleDown;
-
-                            //    downCounter = 0;
-                            //} else {
-                            //    downCounter++;
-                            //}
-                        }
-                    }
-                }
             }
 
-
-            if (!Objects.isNull(widget.getSelectedOrNull())) {
-                if (widget.getSelectedOrNull().equals(this)) color = MicroHudColors.GREEN;
-            }
 
             String text = rendererModule.getName();
             if (!showName) text = rendererModule.render(tickDelta);
             textRenderer.draw(matrices, text, (int) (entryWidth * 0.25 + x), y, color);
-            isMouseClicked = false;
         }
 
 
